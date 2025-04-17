6a32c985ddf25f415c15156c0e12c197be48f402
yasir621 <malikahmad88820@gmail.com>
2025-04-17 19:59:59 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//using namespace std;
+//void printArray(int arr[], int a);
+//int main() {
+//    int arr[5];
+//    cout << "Enter 5 integers" << endl;
+//    for (int i = 0; i < 5; i++)
+//    {
+//        cin >> arr[i];
+//    }
+//    printArray(arr, 5);
+//}
+//void printArray(int arr[], int a) {
+//    for (int i = 0; i < a; i++)
+//        cout <<"Element at postion"<<i<<"="<< arr[i] << " "<<endl;
+//    cout << endl;
+//}
